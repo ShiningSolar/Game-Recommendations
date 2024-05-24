404670b3d9e1758c2f211f6b70746e4f0999e65e
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-05-24 12:45:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ if st.button('Show Recommendation'):
    recommendations = recommend(selected_game)
    #col1, col2, col3, col4, col5 = st.columns(5)
    #cols = st.columns(5)
-   row1, row2, row3, row4, row5 = st.popover
+   #row1, row2, row3, row4, row5 = st.popover
    with st.popover:
       data = recommendations[0]
       st.image(data[1])
