ee99abe37a0c451e0e88d23ee2c9302564ef7343
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-05-26 10:55:52 +0700

    Create game_details.py

---
+++ pages/game_details.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from app import recommendations
+
+data = recommendations
+with st.container(border = True):
+  st.image(data.header_image[0], use_column_width = True)
+  title = data.title[0]
+  with st.popover(title, use_container_width = True):
+    st.text("test")
+         
+with st.container(border = True):
+  st.image(data.header_image[1], use_column_width = True)
+  title = data.title[1]
+  with st.popover(title, use_container_width = True):
+    st.text("test")
+         
+with st.container(border = True):
+  st.image(data.header_image[2], use_column_width = True)
+  title = data.title[2]
+  with st.popover(title, use_container_width = True):
+    st.text("test")
+with st.container(border = True):
+  st.image(data.header_image[3], use_column_width = True)
+  title = data.title[3]
+  with st.popover(title, use_container_width = True):
+    st.text("test")
+with st.container(border = True):
+  st.image(data.header_image[4], use_column_width = True)
+  title = data.title[4]
+  with st.popover(title, use_container_width = True):
+    st.text("test")
