1cb3b057e6e025ee1354553642481bd06f3214dd
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-05-26 09:16:43 +0700

    Update game_directory.py

--- pages/game_directory.py
+++ pages/game_directory.py
@@ -13,3 +13,16 @@ options = st.multiselect(
 )
 
 st.write("You selected:", options)
+
+row1 = st.columns(3)
+row2 = st.columns(3)
+row3 = st.columns(3)
+row4 = st.columns(3)
+list_popular = genre_filtering()
+
+index = 0
+for col in row1 + row2 + row3 + row4:
+   cont = col.container(border = True)
+   cont.image(list_popular.loc[index,'header_image'])
+   cont.text(list_popular.loc[index,'title'])
+   index = index + 1
