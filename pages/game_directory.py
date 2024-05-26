15d560d5e17b235602670b66e060f2be1628d189
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-05-26 09:06:42 +0700

    Update game_directory.py

--- pages/game_directory.py
+++ pages/game_directory.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import panda as pd
+import pandas as pd
 
 st.title("Daftar Game")
 genres = ["Racing", "Adventure", "Sports", "Strategy", "Casual", "RPG", "Simulation", "Action", "Indie"]
