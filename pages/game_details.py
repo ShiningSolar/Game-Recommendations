a8416d1d2138ae5bef7020ee698f971f79022a48
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-05-26 21:21:09 +0700

    Update game_details.py

--- pages/game_details.py
+++ pages/game_details.py
@@ -23,8 +23,8 @@ else:
     else:
         with st.container(border = True):
             st.header('🔴 PILIH GAME TERLEBIH DAHULU 🔴')
-            st.page_link("app.py", label="Halaman Utama", icon="🏠")
-            st.page_link("pages/game_directory.py", label="Daftar Game", icon="🕹")
+            st.page_link("app.py", label="Halaman Utama", icon="🏠", use_container_width = True)
+            st.page_link("pages/game_directory.py", label="Daftar Game", icon="🕹", use_container_width = True)
             #if st.button('Kembali ke Home'):
             #    st.switch_page("app.py")
             #if st.button('Kembali ke Daftar Game'):
