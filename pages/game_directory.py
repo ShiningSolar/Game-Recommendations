a1692d727176d926e1012f03edaa4aad72d08f52
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-05-27 12:07:19 +0700

    Update game_directory.py

--- pages/game_directory.py
+++ pages/game_directory.py
@@ -57,7 +57,7 @@ page_number.markdown(f"""**{num_of_page}**""")
 if back_button.button('Back'):
   st.write('back')
 if next_button.button('Next'):
-  index = str(st.query_params.index)
+  index = int(st.query_params.index)
   st.write('next')
   switch_page(index)
   #index = last_index
