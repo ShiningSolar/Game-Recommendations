af9640774c4d06ea0344c4ac3a5f514c2a03d4e7
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-05-27 10:54:36 +0700

    Update game_directory.py

--- pages/game_directory.py
+++ pages/game_directory.py
@@ -21,19 +21,27 @@ row3 = st.columns(3)
 row4 = st.columns(3)
 list_popular = genre_filtering(options)
 
-index = 0
-for col in row1 + row2 + row3 + row4:
-  cont = col.container(border = True)
-  title = list_popular.loc[index,'title']
-  cont.image(list_popular.loc[index,'header_image'])
-  #cont.text(list_popular.loc[index,'title'])
-  if cont.button(title, use_container_width = True):
-    st.query_params.game_name = title
-    st.switch_page("pages/game_details.py")
-  index = index + 1
+@st.cache_data
+def show_data(list_popular):
+  index = 0
+  for col in row1 + row2 + row3 + row4:
+    cont = col.container(border = True)
+    title = list_popular.loc[index,'title']
+    cont.image(list_popular.loc[index,'header_image'])
+    #cont.text(list_popular.loc[index,'title'])
+    if cont.button(title, use_container_width = True):
+      st.query_params.game_name = title
+      st.switch_page("pages/game_details.py")
+    index = index + 1
+    
+show_data(list_popular)
 
 num_of_item = len(list_popular)
 st.write(num_of_item)
+
+#@st.cache_data
+#def switch_page(num_of_item):
+  
 
 buff1, back_button, page_number, next_button, buff2 = st.columns([3,1,0.5,1,3])
 num_of_page = 1
