9718a96f87c4b257af435b917ac2df5320ba9850
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-05-27 10:40:34 +0700

    Update game_directory.py

--- pages/game_directory.py
+++ pages/game_directory.py
@@ -35,7 +35,7 @@ for col in row1 + row2 + row3 + row4:
 num_of_item = len(list_popular)
 st.write(num_of_item)
 
-buff1, back_button, page_number, next_button, buff2 = st.columns([3,1,1,1,3])
+buff1, back_button, page_number, next_button, buff2 = st.columns([3,1,0.5,1,3])
 num_of_page = 1
 page_number.write(num_of_page)
 if back_button.button('Back'):
