d08b9d85c191b6468920d1b45e8b185f2ecc41e1
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-05-27 10:55:14 +0700

    Update game_directory.py

--- pages/game_directory.py
+++ pages/game_directory.py
@@ -21,7 +21,7 @@ row3 = st.columns(3)
 row4 = st.columns(3)
 list_popular = genre_filtering(options)
 
-@st.cache_data
+
 def show_data(list_popular):
   index = 0
   for col in row1 + row2 + row3 + row4:
