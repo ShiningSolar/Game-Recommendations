7e0b444c47126b7f846a394425a32ce45586ee67
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-06-25 12:08:46 +0700

    Update func_var.py
    add content-based function, change collaborative and hybrid function

--- func_var.py
+++ func_var.py
@@ -12,47 +12,63 @@ cosine_sim_content = pickle.load(open('cosine_sim_content.pkl', 'rb'))
 
 games_title = games.sort_values(by='title')
 
+# Function to get content-based filtering similarity for a given game
+@st.cache_data
+def get_content_based_similarities(game_id):
+   game_idx = games[games['app_id'] == game_id].index[0]
+   
+   # Content-based similarity scores
+   content_sim_scores = list(enumerate(cosine_sim_content[game_idx]))
+   #normalize score
+   max_content_score = max([score for _, score in content_sim_scores])
+   content_sim_scores = [(idx, score / max_content_score) for idx, score in content_sim_scores]
+   
+   return content_sim_scores
+
 # Function to get collaborative filtering similarity for a given game
 @st.cache_data
 def get_collaborative_similarities(game_id, k=10):
+   # Mendapatkan index game dengan menyamakan 'app_id' dengan nilai game_id
    game_idx = games[games['app_id'] == game_id].index[0]
-   game_vector = games_sparse[game_idx, :]
+   game_vector = urm_sparse[game_idx, :]
+    
+   # Mencari tetangga terdekat
    distances, indices = knn.kneighbors(game_vector, n_neighbors=k)
-   return indices.flatten(), distances.flatten()
+   # Collaborative similarity scores
+   collab_sim_scores = [(idx, 1 - dist) for idx, dist in zip(indices.flatten(), distances.flatten())]
+   
+   # normalize score
+   max_collab_score = max([score for _, score in collab_sim_scores])
+   collab_sim_scores = [(idx, score / max_collab_score) for idx, score in collab_sim_scores]
+   
+   return collab_sim_scores
 
 # 4. Hybrid Filtering
 @st.cache_data
 def hybrid_recommendation(input_game_title, k = 10):
-   # Find the game ID for the input title
+   # Mendapatkan ID game untuk judul masukan
    input_game_id = games[games['title'] == input_game_title]['app_id'].values[0] 
+   # Mendapatkan index game untuk judul masukan
    input_game_idx = games[games['app_id'] == input_game_id].index[0]
-   #games[games['title'].str.contains(input_game_title, case=False)].iloc[0]['app_id']
    
-   # Content-based similarity scores
-   content_sim_scores = list(enumerate(cosine_sim_content[input_game_idx]))
-   
-   # Collaborative similarity scores
-   collab_indices, collab_distances = get_collaborative_similarities(input_game_id, k+1)
-   collab_sim_scores = [(idx, 1 - dist) for idx, dist in zip(collab_indices, collab_distances)]
-   
-   # Normalize scores
-   max_content_score = max([score for _, score in content_sim_scores])
-   max_collab_score = max([score for _, score in collab_sim_scores])
-   content_sim_scores = [(idx, score / max_content_score) for idx, score in content_sim_scores]
-   collab_sim_scores = [(idx, score / max_collab_score) for idx, score in collab_sim_scores]
+   # similarity scores
+   content_sim_scores = get_content_based_similarities(input_game_id)
+   collab_sim_scores = get_collaborative_similarities(input_game_id, k+1)
    
    # Combine scores with a weighted average
    combined_scores = {}
    for idx, score in content_sim_scores:
-      combined_scores[idx] = combined_scores.get(idx, 0) + 0.5 * score
+       combined_scores[idx] = combined_scores.get(idx, 0) + 0.5 * score
    for idx, score in collab_sim_scores:
-      combined_scores[idx] = combined_scores.get(idx, 0) + 0.5 * score
-   
+       combined_scores[idx] = combined_scores.get(idx, 0) + 0.5 * score
+    
    # Sort and get top k recommendations
    sorted_games = sorted(combined_scores.items(), key=lambda x: x[1], reverse=True)
    recommended_game_indices = [idx for idx, _ in sorted_games[1:k+1]]
+   
    # Retrieve game titles
    recommended_games = games.iloc[recommended_game_indices].reset_index(drop=True)
+   
    return recommended_games
 
 def sorted_score_df(df):
