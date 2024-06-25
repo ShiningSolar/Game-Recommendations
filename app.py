a7f7614f64f75c56b19bc0d70c5adefb0c286f71
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-06-26 00:46:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.neighbors import NearestNeighbors
+import streamlit_antd_components as sac
 from func_var import games_title
-from func_var import hybrid_recommendation
 from func_var import most_popular_games
 
 st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
+    page_title="Halaman Utama",
     page_icon="🧊",
     layout="wide",
     initial_sidebar_state="auto",
@@ -18,6 +16,24 @@ st.set_page_config(
         'About': "# This is a header. This is an *extremely* cool app!"
     }
 )
+
+sac.menu([
+    sac.MenuItem('home', icon='house-fill', tag=[sac.Tag('Tag1', color='green'), sac.Tag('Tag2', 'red')]),
+    sac.MenuItem('products', icon='box-fill', children=[
+        sac.MenuItem('apple', icon='apple'),
+        sac.MenuItem('other', icon='git', description='other items', children=[
+            sac.MenuItem('google', icon='google', description='item description'),
+            sac.MenuItem('gitlab', icon='gitlab'),
+            sac.MenuItem('wechat', icon='wechat'),
+        ]),
+    ]),
+    sac.MenuItem('disabled', disabled=True),
+    sac.MenuItem(type='divider'),
+    sac.MenuItem('link', type='group', children=[
+        sac.MenuItem('antd-menu', icon='heart-fill', href='https://ant.design/components/menu#menu'),
+        sac.MenuItem('bootstrap-icon', icon='bootstrap-fill', href='https://icons.getbootstrap.com/'),
+    ]),
+], open_all=True)
 
 st.title("Aplikasi Rekomendasi Game")
 
