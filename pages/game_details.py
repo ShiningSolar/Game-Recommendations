24f4569c0027818599344bda352d62f20f00f882
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-06-25 22:40:58 +0700

    Update game_details.py

--- pages/game_details.py
+++ pages/game_details.py
@@ -35,7 +35,7 @@ def detail_selected_game(game_name, details):
         st.image(details.loc['header_image'], use_column_width = True)
         st.header(details.loc['title'])
         sac.divider(label='Genre', align='center', color='gray')
-        sac.buttons(genres, label='', index=None, align='center', radius='lg', gap=6, variant='filled', color='dark')
+        sac.buttons(genres, label='', index=None, align='center', radius='lg', gap='xs', variant='filled', color='dark')
 
 def view(game_name):
     recommendations = hybrid_recommendation(game_name)
