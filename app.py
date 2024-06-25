53600a907b0670edaa7eecee3810d9f63ff27636
ShiningSolar <144994505+ShiningSolar@users.noreply.github.com>
2024-06-26 00:48:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,23 +17,24 @@ st.set_page_config(
     }
 )
 
-sac.menu([
-    sac.MenuItem('home', icon='house-fill', tag=[sac.Tag('Tag1', color='green'), sac.Tag('Tag2', 'red')]),
-    sac.MenuItem('products', icon='box-fill', children=[
-        sac.MenuItem('apple', icon='apple'),
-        sac.MenuItem('other', icon='git', description='other items', children=[
-            sac.MenuItem('google', icon='google', description='item description'),
-            sac.MenuItem('gitlab', icon='gitlab'),
-            sac.MenuItem('wechat', icon='wechat'),
+with st.sidebar:
+    sac.menu([
+        sac.MenuItem('home', icon='house-fill', tag=[sac.Tag('Tag1', color='green'), sac.Tag('Tag2', 'red')]),
+        sac.MenuItem('products', icon='box-fill', children=[
+            sac.MenuItem('apple', icon='apple'),
+            sac.MenuItem('other', icon='git', description='other items', children=[
+                sac.MenuItem('google', icon='google', description='item description'),
+                sac.MenuItem('gitlab', icon='gitlab'),
+                sac.MenuItem('wechat', icon='wechat'),
+            ]),
         ]),
-    ]),
-    sac.MenuItem('disabled', disabled=True),
-    sac.MenuItem(type='divider'),
-    sac.MenuItem('link', type='group', children=[
-        sac.MenuItem('antd-menu', icon='heart-fill', href='https://ant.design/components/menu#menu'),
-        sac.MenuItem('bootstrap-icon', icon='bootstrap-fill', href='https://icons.getbootstrap.com/'),
-    ]),
-], open_all=True)
+        sac.MenuItem('disabled', disabled=True),
+        sac.MenuItem(type='divider'),
+        sac.MenuItem('link', type='group', children=[
+            sac.MenuItem('antd-menu', icon='heart-fill', href='https://ant.design/components/menu#menu'),
+            sac.MenuItem('bootstrap-icon', icon='bootstrap-fill', href='https://icons.getbootstrap.com/'),
+        ]),
+    ], open_all=True)
 
 st.title("Aplikasi Rekomendasi Game")
 
