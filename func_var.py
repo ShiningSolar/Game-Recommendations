d6c782ec7843b39e56e0c406f2f1ef2f9b5d092b
Sisy Agnia <45039754+ssygnrchm@users.noreply.github.com>
2024-07-08 11:51:50 +0700

    almost done extra

--- func_var.py
+++ func_var.py
@@ -1,76 +1,78 @@
 import streamlit as st
 import pickle
-import pandas as pd
-import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.neighbors import NearestNeighbors
 
 games = pickle.load(open('games.pkl', 'rb'))
-games_sparse = pickle.load(open('urm_sparse.pkl', 'rb'))
-knn = pickle.load(open('knn.pkl', 'rb'))
 cosine_sim_content = pickle.load(open('cosine_sim_content.pkl', 'rb'))
+cosine_sim_collab = pickle.load(open('cosine_sim_collab.pkl', 'rb'))
 
+genres = ["Racing", "Adventure", "Sports", "Strategy", "Casual", "RPG", "Simulation", "Action", "Indie"]
 games_title = games.sort_values(by='title')
 
-# Function to get content-based filtering similarity for a given game
+# Fungsi untuk mendapatkan kesamaan content-based filtering untuk game tertentu
 @st.cache_data
-def get_content_based_similarities(game_id):
-   game_idx = games[games['app_id'] == game_id].index[0]
-   
-   # Content-based similarity scores
+def get_content_based_similarities(game_idx, k):
+   # Mendapatkan skor kesamaan konten untuk game_idx
    content_sim_scores = list(enumerate(cosine_sim_content[game_idx]))
-   #normalize score
+   # Normalisasi skor kesamaan ke rentang 0-1
    max_content_score = max([score for _, score in content_sim_scores])
    content_sim_scores = [(idx, score / max_content_score) for idx, score in content_sim_scores]
-   
-   return content_sim_scores
 
-# Function to get collaborative filtering similarity for a given game
+   # Mengurutkan skor kesamaan berdasarkan nilai skor dalam urutan menurun
+   sorted_content_sim_scores = sorted(content_sim_scores, key=lambda x: x[1], reverse=True)
+   # Mengambil top k skor kesamaan, dimulai dari indeks 1 (skip yang pertama karena itu adalah game_id itu sendiri)
+   top_content_sim_scores = sorted_content_sim_scores[1:k + 1]  # Mengambil dari indeks 1 hingga top_n+1
+   # Mengembalikan daftar top k skor kesamaan konten
+   return top_content_sim_scores
+
+# Fungsi untuk mendapatkan kesamaan collaborative filtering untuk game tertentu
 @st.cache_data
-def get_collaborative_similarities(game_id, k=10):
-   # Mendapatkan index game dengan menyamakan 'app_id' dengan nilai game_id
-   game_idx = games[games['app_id'] == game_id].index[0]
-   game_vector = games_sparse[game_idx, :]
-    
-   # Mencari tetangga terdekat
-   distances, indices = knn.kneighbors(game_vector, n_neighbors=k)
-   # Collaborative similarity scores
-   collab_sim_scores = [(idx, 1 - dist) for idx, dist in zip(indices.flatten(), distances.flatten())]
+def get_collaborative_similarities(game_idx, k):
+   # Mendapatkan skor kesamaan kolaboratif untuk game_idx
+   collab_sim_scores = list(enumerate(cosine_sim_collab[game_idx]))
    
-   # normalize score
+   # Normalisasi skor kesamaan ke rentang 0-1
    max_collab_score = max([score for _, score in collab_sim_scores])
    collab_sim_scores = [(idx, score / max_collab_score) for idx, score in collab_sim_scores]
-   
-   return collab_sim_scores
 
-# 4. Hybrid Filtering
+   # Mengurutkan skor kesamaan berdasarkan nilai skor dalam urutan menurun
+   sorted_collab_sim_scores = sorted(collab_sim_scores, key=lambda x: x[1], reverse=True)
+
+   # Mengambil top k skor kesamaan, dimulai dari indeks 1 (skip yang pertama karena itu adalah game_id itu sendiri)
+   top_collab_sim_scores = sorted_collab_sim_scores[1:k + 1]  # Mengambil dari indeks 1 hingga top_n+1
+    
+   # Mengembalikan daftar top k skor kesamaan kolaboratif
+   return top_collab_sim_scores
+
+# Fungsi untuk mendapatkan rekomendasi game dari content-based dan collaborative filtering
 @st.cache_data
-def hybrid_recommendation(input_game_title, k = 10):
-   # Mendapatkan ID game untuk judul masukan
+def hybrid_recommendation(input_game_title, k = 12):
+   # Mendapatkan ID game untuk judul game yang diinput
    input_game_id = games[games['title'] == input_game_title]['app_id'].values[0] 
-   # Mendapatkan index game untuk judul masukan
+   # Mendapatkan index game untuk judul game yang diinput
    input_game_idx = games[games['app_id'] == input_game_id].index[0]
    
-   # similarity scores
-   content_sim_scores = get_content_based_similarities(input_game_id)
-   collab_sim_scores = get_collaborative_similarities(input_game_id, k+1)
+   # Mendapatkan skor kesamaan content-based dan collaborative
+   content_sim_scores = get_content_based_similarities(input_game_idx, k)
+   collab_sim_scores = get_collaborative_similarities(input_game_idx, k)
    
-   # Combine scores with a weighted average
+   # Menggabungkan skor dengan bobot yang sama (0.5)
    combined_scores = {}
    for idx, score in content_sim_scores:
        combined_scores[idx] = combined_scores.get(idx, 0) + 0.5 * score
    for idx, score in collab_sim_scores:
        combined_scores[idx] = combined_scores.get(idx, 0) + 0.5 * score
     
-   # Sort and get top k recommendations
+   # Mengurutkan game berdasarkan skor gabungan dan mengambil indeks top k game
    sorted_games = sorted(combined_scores.items(), key=lambda x: x[1], reverse=True)
-   recommended_game_indices = [idx for idx, _ in sorted_games[1:k+1]]
+   recommended_game_indices = [idx for idx, _ in sorted_games[:k]]
    
-   # Retrieve game titles
+   # Mengambil judul game dari indeks yang direkomendasikan
    recommended_games = games.iloc[recommended_game_indices].reset_index(drop=True)
    
+   # Mengembalikan DataFrame berisi game yang direkomendasikan
    return recommended_games
 
+# Mengurutkan DataFrame berdasarkan skor
 def sorted_score_df(df):
    df['score'] = df['user_reviews'] * df['positive_ratio']
    df_sorted = df.sort_values('score', ascending=False)
@@ -79,7 +81,7 @@ def sorted_score_df(df):
 @st.cache_data
 def most_popular_games():
    df = games
-   n = 12 #number of games
+   n = 12 #jumlah game
    list = sorted_score_df(df).reset_index(drop = True)
    return list.iloc[0:n]
 
@@ -89,7 +91,7 @@ def genre_filtering(list_genre = []):
    if len(list_genre) != 0:
       for genre in list_genre:
          genres_games = df[df['genres'].apply(lambda x: all(item in x for item in list_genre))]
-         #df = genres_games
+         
       filtered_games = sorted_score_df(genres_games).reset_index(drop = True)
    else :
       filtered_games = df.sort_values('title', ascending=True).reset_index(drop = True)
@@ -100,4 +102,35 @@ def selected_game_details(game_name):
    game_id = games[games['title'] == game_name]['app_id'].values[0] 
    game_idx = games[games['app_id'] == game_id].index[0]
    list_details = games.loc[game_idx]
-   return list_details
+
+   title = list_details.loc['title']
+   genres = list_details.loc['genres']
+   tanggal = str(list_details.loc['date_release'])
+   website = list_details.loc['website']
+   screenshots = list_details.loc['screenshots']
+   if list_details.loc['movies'] != 'Unknown':
+      movies = list_details.loc['movies']
+      movies = movies[0]
+      movies_thumbnail = ["asset/video.jpg"]
+      list_media = movies_thumbnail + screenshots
+   else:
+      movies = list_details.loc['movies']
+      list_media = screenshots
+   about = list_details.loc['about']
+   review = str(list_details.loc['rating'])+' ('+str(list_details.loc['user_reviews'])+')'
+   developer = list_details['developers'].split(',')
+   publisher = list_details['publishers'].split(',')
+   header_img = list_details.loc['header_image']
+
+   return title, genres, tanggal, website, movies, list_media, about, review, developer, publisher, header_img
+
+@st.cache_data
+def get_recommendation_data(game_name):
+    recommendations = hybrid_recommendation(game_name)
+    title, genres, tanggal, website, movies, list_media, about, review, developer, publisher, header_img = selected_game_details(game_name)
+    return recommendations, title, genres, tanggal, website, movies, list_media, about, review, developer, publisher, header_img
+
+def change_page(name):
+    st.session_state['saved_name'] = name
+    st.query_params.game_name = st.session_state['saved_name']
+    st.session_state['details_page'] = True
--- 🏠_Beranda.py
+++ 🏠_Beranda.py
@@ -1,48 +1,59 @@
+# Mengimpor library Streamlit untuk membangun antarmuka web
 import streamlit as st
-import pandas as pd
-import numpy as np
-import streamlit_antd_components as sac
-from func_var import games_title
-from func_var import most_popular_games
-
+# Mengimpor modul kustom dari file bernama func_var.py
+from func_var import games_title, most_popular_games
+# Mengatur konfigurasi halaman Streamlit
 st.set_page_config(
-    page_title="Beranda",
-    page_icon="🏠",
-    layout="wide",
-    initial_sidebar_state="auto"
+    page_title="Beranda", # Judul halaman
+    page_icon="🏠", # Ikon halaman
+    layout="wide", # Layout diatur ke "wide" untuk tampilan lebar
+    initial_sidebar_state="auto" # Sidebar diatur untuk terbuka otomatis
 )
-
+# Menampilkan judul utama
 st.title("Aplikasi Rekomendasi Game")
-
-selected_game = st.selectbox(
-   "Type or select a game",
-   games_title['title'],
-   index = None,
-   placeholder = "Ketik atau pilih game",
-   label_visibility = "collapsed"
-)
-
-col1, col2, col3 = st.columns([2, 1, 2])
+st.divider()
+# Membuat wadah untuk search box
+con = st.container(border=True)
+con.markdown('Pencarian rekomendasi game')
+# Membagi wadah menjadi dua kolom dengan rasio 3:1
+col1, col2 = con.columns([3,1])
+with col1:
+    # Membuat kotak pilihan (selectbox) yang berisi daftar judul game dari games_title['title']
+    # Pengguna dapat memilih atau mengetikkan judul game
+    selected_game = st.selectbox(
+        "Type or select a game",
+        games_title['title'],
+        index = None,
+        placeholder = "Ketik atau pilih game",
+        label_visibility = "collapsed")
 with col2:
-    if st.button('Cari rekomendasi'):
-       st.query_params.game_name = selected_game
-       st.switch_page("pages/game_details.py")
-
-st.header("Game Terpopuler")
-row1 = st.columns(3)
-row2 = st.columns(3)
-row3 = st.columns(3)
-row4 = st.columns(3)
-list_popular = most_popular_games()
-
-index = 0
-for col in row1 + row2 + row3 + row4:
-    image = list_popular.loc[index,'header_image']
-    title = list_popular.loc[index,'title']
-    cont = col.container(border = True)
-    cont.image(image)
-    if cont.button(title, use_container_width = True):
-        st.query_params.game_name = title
+    # Membuat tombol "Cari rekomendasi"
+    if st.button('Cari rekomendasi', use_container_width=True):
+        # Jika tomblo diklik maka judul game akan disimpan pada query URL
+        # dan halaman akan beralih ke halaman rekomendasi game
+        st.query_params.game_name = selected_game
         st.switch_page("pages\🎮_Rekomendasi_Game.py")
-    index = index + 1
+# Game Terpopuler
+# Membuat wadah untuk menampilkan game terpopuler
+with st.container(border=True):
+    st.markdown("Game terpopuler")
+    row1 = st.columns(3)
+    row2 = st.columns(3)
+    row3 = st.columns(3)
+    row4 = st.columns(3)
+    # Memanggil fungsi most_popular_games() untuk mendapatkan daftar game terpopuler
+    list_popular = most_popular_games()
+    # Loop untuk menampilkan game terpopuler
+    index = 0
+    for col in row1 + row2 + row3 + row4:
+        image = list_popular.loc[index,'header_image']
+        title = list_popular.loc[index,'title']
+        cont = col.container(border = True)
+        cont.image(image)
+        # Membuat tombol untuk beralih ke halaman rekomendasi game
+        # sesuai dengan judul game yang dipilih
+        if cont.button(title, use_container_width = True):
+            st.query_params.game_name = title
+            st.switch_page("pages\🎮_Rekomendasi_Game.py")
+        index = index + 1
 
